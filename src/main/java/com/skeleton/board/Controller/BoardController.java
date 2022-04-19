f852f58a70f0c1dbb3a63e0921850b000f8ad426
kingle1024 <teran1024@naver.com>
2022-04-19 22:45:57 +0900

    Initial commit

---
+++ src/main/java/com/skeleton/board/Controller/BoardController.java
@@ -0,0 +1,18 @@
+package com.skeleton.board.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    @GetMapping("/board")
+    public String board(Model model){
+
+        model.addAttribute("list", "list");
+
+        return "/board";
+    }
+
+
+}
